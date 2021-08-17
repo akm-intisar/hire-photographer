1b23c36caaba607f9ace2ab5d66eed20d13e3702
AKM Intisar Islam <akm.intisar@gmail.com>
2021-08-17 23:10:42 +0600

    bootstrapAdded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import homepagePhoto from './Images/photo-1.jpg'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hire Photographer As Your Budget</h1>
+        <img src={homepagePhoto} alt=""></img>
+    </div>
+  );
+}
+
+export default App;
