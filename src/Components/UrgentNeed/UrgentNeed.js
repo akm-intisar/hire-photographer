e94861eace869e96412b709596110e218a9d8b8c
AKM Intisar Islam <akm.intisar@gmail.com>
2021-08-25 23:58:49 +0600

    urgent-page

--- src/Components/UrgentNeed/UrgentNeed.js
+++ src/Components/UrgentNeed/UrgentNeed.js
@@ -1,13 +1,25 @@
-import { Alert } from 'bootstrap';
-import React from 'react';
+import { Alert } from "bootstrap";
+import React from "react";
 
 const UrgentNeed = () => {
-    return (
-        <div>
-            <h1>Call Us</h1>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Call Us</h1>
+      <Alert variant="success">
+        <Alert.Heading>Hey, nice to see you</Alert.Heading>
+        <p>
+          Aww yeah, you successfully read this important alert message. This
+          example text is going to run a bit longer so that you can see how
+          spacing within an alert works with this kind of content.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
+        </p>
+      </Alert>
+    </div>
+  );
 };
 
 export default UrgentNeed;
